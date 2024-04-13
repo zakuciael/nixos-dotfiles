8d96af48719a60981719c0d400813764a208b955
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-04-13 08:50:39 +0200

    feat(wip): add commit signing and 1pass ssh agent

--- hosts/pc/home.nix
+++ hosts/pc/home.nix
@@ -21,4 +21,25 @@
     enable = true;
     nix-direnv.enable = true;
   };
+
+  # TODO: Move to separate module
+  programs.git = {
+    enable = true;
+    userName = "Krzysztof Saczuk";
+    userEmail = "me@krzysztofsaczuk.pl";
+    extraConfig = {
+      commit.gpgsign = true;
+      gpg.format = "ssh";
+      gpg.ssh.program = "${pkgs._1password-gui}/bin/op-ssh-sign";
+      user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEcrcFZPwdfoZb0ZP3SUr/ZgN6Hycpk57Ky1UMmPbAg8";
+    };
+  };
+
+  programs.ssh = let agentPath = "~/.1password/agent.sock"; in {
+    enable = true;
+    extraConfig = ''
+      Host *
+          IdentityAgent ${agentPath}
+    '';
+  };
 }
