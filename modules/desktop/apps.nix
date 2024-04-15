894b5c639c639229f99c2a928e677622bc4c9a87
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-04-15 08:10:07 +0200

    chore(apps): install vesktop

--- modules/desktop/apps.nix
+++ modules/desktop/apps.nix
@@ -19,6 +19,7 @@ in {
       programs.fish.shellAliases.open = "xdg-open";
       home.packages = with pkgs; [
         discord
+        vesktop
         vscode
         google-chrome
         warp-terminal
