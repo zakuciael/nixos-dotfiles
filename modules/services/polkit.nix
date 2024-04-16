5d9219dd65812638a0eaf91822ca70bc0bae4f42
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-04-16 17:36:31 +0200

    feat(services): add polkit authentication manager

---
+++ modules/services/polkit.nix
@@ -0,0 +1,36 @@
+{
+  pkgs,
+  lib,
+  config,
+  home-manager,
+  username,
+  ...
+}:
+with lib;
+with lib.my; let
+  cfg = config.modules.services.polkit;
+in {
+  options.modules.services.polkit = {
+    enable = mkEnableOption "Enable polkit authentication manager";
+  };
+
+  config = mkIf (cfg.enable) {
+    security.polkit.enable = true;
+
+    home-manager.users.${username}.home.packages = with pkgs; [polkit_gnome];
+
+    systemd.user.services."polkit-gnome-authentication-agent-1" = {
+      description = "Polkit authentication manager";
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+}
