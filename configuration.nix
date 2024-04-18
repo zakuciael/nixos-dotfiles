872eb86a304a0d3376567851f44daf67ff05a3f1
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-04-18 19:05:45 +0200

    feat(nix): enable store optimization

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
     settings = {
       max-jobs = 6;
       cores = 6;
+      auto-optimise-store = true;
       substituters = ["https://hyprland.cachix.org"];
       trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
       experimental-features = ["nix-command" "flakes"];
