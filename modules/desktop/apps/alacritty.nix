3f156b15821f04f3a8ade66479f4b55c7d541f61
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-04-19 09:18:36 +0200

    feat(alacritty): add color scheme settings

--- modules/desktop/apps/alacritty.nix
+++ modules/desktop/apps/alacritty.nix
@@ -1,8 +1,13 @@
 {
   pkgs,
   username,
+  config,
+  lib,
   ...
-}: {
+}:
+with lib; let
+  colorScheme = config.home-manager.users.${username}.colorScheme;
+in {
   home-manager.users.${username}.programs.alacritty = {
     enable = true;
     settings = {
@@ -11,6 +16,36 @@
         "live_config_reload" = false;
       };
       env.TERM = "xterm-256color";
+      colors = mkIf (colorScheme.author != "") (with colorScheme.colors; {
+        bright = {
+          black = "0x${base00}";
+          blue = "0x${base0D}";
+          cyan = "0x${base0C}";
+          green = "0x${base0B}";
+          magenta = "0x${base0E}";
+          red = "0x${base08}";
+          white = "0x${base06}";
+          yellow = "0x${base09}";
+        };
+        cursor = {
+          cursor = "0x${base06}";
+          text = "0x${base06}";
+        };
+        normal = {
+          black = "0x${base00}";
+          blue = "0x${base0D}";
+          cyan = "0x${base0C}";
+          green = "0x${base0B}";
+          magenta = "0x${base0E}";
+          red = "0x${base08}";
+          white = "0x${base06}";
+          yellow = "0x${base0A}";
+        };
+        primary = {
+          background = "0x${base00}";
+          foreground = "0x${base06}";
+        };
+      });
     };
   };
 }
