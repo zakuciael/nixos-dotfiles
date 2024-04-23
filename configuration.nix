3702d307072bbc257348a37f0fe77c43a512bdf3
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-04-23 10:13:43 +0200

    feat: add plymouth

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,37 @@
   username,
   inputs,
   ...
-}: {
+}: let
+  inherit (pkgs) stdenv fetchFromGitea;
+  nixos-blur-playmouth = stdenv.mkDerivation {
+    name = "nixos-blur-plymouth";
+
+    src = fetchFromGitea {
+      domain = "git.gurkan.in";
+      owner = "gurkan";
+      repo = "nixos-blur-plymouth";
+      rev = "ea75b51a1f04aa914647a2929eab6bbe595bcfc0";
+      sha256 = "sha256-BSmh+Gy3yJMA4RoJ0uaQ/WsYBs+Txr6K3cAQjf+yM5Y=";
+    };
+
+    installPhase = ''
+      mkdir -p $out/share/plymouth/themes/nixos-blur
+
+      cp -r ./nixos-blur/* $out/share/plymouth/themes/nixos-blur
+
+      chmod +x $out/share/plymouth/themes/nixos-blur/nixos-blur.plymouth
+      chmod +x $out/share/plymouth/themes/nixos-blur/nixos-blur.script
+    '';
+
+    meta = with lib; {
+      homepage = "https://git.gurkan.in/gurkan/nixos-blur-plymouth";
+      description = "Blue plymouth theme created by gurkan";
+      license = licenses.gpl3;
+      maintainers = with maintainers; [Wittano];
+      platforms = platforms.linux;
+    };
+  };
+in {
   # NixOS configuration
   nix = {
     settings = {
@@ -78,6 +108,12 @@
       cleanOnBoot = true;
       useTmpfs = true;
       tmpfsSize = "25%";
+    };
+
+    plymouth = {
+      enable = true;
+      themePackages = [nixos-blur-playmouth];
+      theme = "nixos-blur";
     };
   };
 
