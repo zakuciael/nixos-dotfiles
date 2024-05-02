2a3fe58d4d1ad1898890397d35304760be844cb8
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-05-02 17:59:11 +0200

    refactor(modules): rename `monitor-layout` to `layout`

---
+++ lib/defs.nix
@@ -0,0 +1,22 @@
+{lib, ...}:
+with lib;
+with lib.types; {
+  monitor = mkOption {
+    description = "Monitor definition for diffrent window servers.";
+    example = {};
+    type = submodule {
+      options = {
+        xorg = mkOption {
+          description = "Name of the monitor on the Xorg window server.";
+          example = "DisplayPort-0";
+          type = str;
+        };
+        wayland = mkOption {
+          description = "Name of the monitor on the Wayland window server.";
+          example = "DP-1";
+          type = str;
+        };
+      };
+    };
+  };
+}
