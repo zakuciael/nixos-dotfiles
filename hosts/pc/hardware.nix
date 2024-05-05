fd49594459e5b3803bf4f2a118a0adfb542a97d5
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-05-05 15:04:59 +0200

    feat(pc): add storage RAID1 array (#53)

--- hosts/pc/hardware.nix
+++ hosts/pc/hardware.nix
@@ -23,6 +23,10 @@
     device = "/dev/disk/by-partlabel/linux-games";
   };
 
+  fileSystems."/media/storage" = {
+    device = "/dev/md/storage";
+  };
+
   # TODO: Temp, remove when full transision to NixOS is done.
   fileSystems."/media/arch" = {
     device = "/dev/disk/by-uuid/0db30fcb-b052-4ef0-9c08-1382d82b4eb5";
@@ -32,7 +36,8 @@
 
   boot.initrd.kernelModules = ["kvm-intel"];
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.swraid.enable = true;
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  nixpkgs.hostPlatform = lib.mkDefault pkgs.system;
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
