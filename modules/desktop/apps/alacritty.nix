e3e61c73b05719e8f16ac506821d7a0f8a2d3112
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-05-31 06:53:31 +0200

    feat(alacritty): add window styling

--- modules/desktop/apps/alacritty.nix
+++ modules/desktop/apps/alacritty.nix
@@ -14,6 +14,25 @@
           shell = "${pkgs.fish}/bin/fish";
           "live_config_reload" = false;
         };
+        window = {
+          padding = {
+            x = 10;
+            y = 10;
+          };
+          opacity = 0.9;
+          blur = true;
+          dynamic_padding = true;
+          dynamic_title = true;
+        };
+        scrolling.history = 100000;
+        font = {
+          normal = {
+            family = "JetBrainsMono Nerd Font Mono";
+            style = "Regular";
+          };
+          size = 11;
+          builtin_box_drawing = true;
+        };
         env.TERM = "xterm-256color";
       };
     };
