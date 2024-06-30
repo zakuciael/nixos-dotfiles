4e3f84bb06000f9c83d3ad8e6d67af32e2938925
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-06-30 19:28:11 +0200

    refactor(lib): remove unused `toCfg` mapper

--- lib/mapper.nix
+++ lib/mapper.nix
@@ -53,53 +53,6 @@ with lib.my.utils; rec {
 
   toJSON = name: attrs: (pkgs.formats.json {}).generate name attrs;
 
-  toCfg = name: attrs: let
-    # This script fix problem for nitrogen, because for some reason
-    # nitrogen read path with " chars and throws exception for that
-    fixCfgPaths =
-      builtins.toFile "fixCfgPaths.py"
-      /*
-      python
-      */
-      ''
-        #!/usr/bin/env python
-        from typing import List
-        import sys
-
-        if len(sys.argv) < 2:
-          raise AssertionError("Missing required input file")
-
-        content: List[str] = []
-        inputFile = sys.argv[1]
-
-        with open(inputFile, "r") as f:
-            for line in f:
-                content.append(line)
-
-        if len(content) <= 0:
-            raise AssertionError("file is empty")
-
-        with open(inputFile, "w") as f:
-            for i, line in enumerate(content):
-                if "/" in line:
-                    content[i] = line.replace("\"", "")
-
-            f.writelines(content)
-      '';
-  in
-    (toTOML name attrs).overrideAttrs
-    (final: prev: {
-      nativeBuildInputs = prev.nativeBuildInputs ++ [pkgs.python3];
-      buildCommand =
-        /*
-        bash
-        */
-        ''
-          json2toml "$valuePath" $out
-          python ${fixCfgPaths} $out
-        '';
-    });
-
   toRasiKeyValue = {indent ? ""}: name: value:
     if isRasiSection value
     then toRasiSection {inherit indent;} name value # Sections
