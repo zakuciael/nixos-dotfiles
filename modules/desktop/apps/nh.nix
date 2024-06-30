cbe2cc1d90cfdd8811f4cbdc0703aee57e9e6f40
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-06-30 19:04:13 +0200

    fix(services): nh-clean service not launching due to timer config

--- modules/desktop/apps/nh.nix
+++ modules/desktop/apps/nh.nix
@@ -1,11 +1,22 @@
-{unstable, ...}: {
+{
+  lib,
+  unstable,
+  ...
+}:
+with lib; {
   programs.nh = {
     enable = true;
     package = unstable.nh;
     clean = {
       enable = true;
-      dates = "weekly";
+      dates = "daily";
       extraArgs = "--keep 10 --keep-since 7d";
     };
+  };
+
+  systemd.timers.nh-clean.timerConfig = mkForce {
+    OnBootSec = "15m";
+    OnUnitActiveSec = "1d";
+    Persistent = true;
   };
 }
