925ff2cd0b144d3458aa4ea121f3216a1bb94369
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-08-07 07:55:04 +0200

    feat(gaming): add minecraft launcher

---
+++ modules/desktop/gaming/minecraft.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  lib,
+  pkgs,
+  username,
+  ...
+}:
+with lib;
+with lib.my;
+with lib.my.utils; let
+  cfg = config.modules.desktop.gaming.minecraft;
+  layout = findLayoutConfig config ({index, ...}: index == 1); # Main monitor
+  monitor = getLayoutMonitor layout "wayland";
+  minecraftClass = "(Minecraft)";
+in {
+  options.modules.desktop.gaming.minecraft = {
+    enable = mkEnableOption "minecraft";
+  };
+
+  config = mkIf (cfg.enable) {
+    home-manager.users.${username} = {
+      home.packages = with pkgs; [prismlauncher];
+
+      wayland.windowManager.hyprland.settings = {
+        windowrulev2 = [
+          "float, class:${minecraftClass}"
+          "monitor ${monitor}, class:${minecraftClass}"
+          "center, class:${minecraftClass}"
+          "maximize, class:${minecraftClass}"
+        ];
+      };
+    };
+  };
+}
