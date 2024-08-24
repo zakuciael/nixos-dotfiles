36629d246beacbe909f8bb351cad5706b446f3aa
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-08-24 23:17:32 +0200

    refactor(hardware): generate YubiKey's `u2f_keys` file from secrets

--- modules/hardware/yubikey.nix
+++ modules/hardware/yubikey.nix
@@ -1,23 +1,33 @@
 {
   config,
   lib,
+  username,
   ...
 }:
 with lib;
 with lib.my; let
   cfg = config.modules.hardware.yubikey;
+  hmConfig = config.home-manager.users.${username};
+  configDirectory = hmConfig.xdg.configHome;
 in {
   options.modules.hardware.yubikey = {
     enable = mkEnableOption "YubiKey support";
   };
 
   config = mkIf (cfg.enable) {
-    # TODO: Add ~/.config/Yubico/u2f_keys to repo when secret managment is in place
+    sops.secrets = {
+      "users/${username}/u2f_keys" = {
+        mode = "0644";
+        owner = username;
+        path = "${configDirectory}/Yubico/u2f_keys";
+      };
+    };
 
     security.pam = {
       u2f = {
         enable = true;
         cue = true;
+        interactive = true;
         control = "sufficient";
       };
 
