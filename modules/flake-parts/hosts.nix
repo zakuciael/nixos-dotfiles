b66b2b101b097ede62ddda46db13c628a721c973
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-11-10 00:11:04 +0100

    refactor: add overlays only if flake has them defined

--- modules/flake-parts/hosts.nix
+++ modules/flake-parts/hosts.nix
@@ -1,9 +1,12 @@
 {
+  lib,
   self,
   inputs,
   withSystem,
   ...
 }: let
+  inherit (lib) mkMerge optionalAttrs hasAttr;
+
   mkHost = name:
     withSystem "x86_64-linux" ({
       self',
@@ -20,8 +23,8 @@
           hostname = name;
         };
 
-        modules = [
-          {nixpkgs.overlays = [self.overlays.default];}
+        modules = mkMerge [
+          (optionalAttrs (hasAttr "default" self.overlays) {nixpkgs.overlays = [self.overlays.default];})
 
           ./../../hosts/${name}/configuration.nix
         ];
