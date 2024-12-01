0e66644128b1613c81c290415c85d7b6069b27c5
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-12-01 18:29:57 +0100

    refactor(gaming): open firewall ports for steam local game transfers

--- modules/desktop/gaming/steam.nix
+++ modules/desktop/gaming/steam.nix
@@ -26,6 +26,7 @@ in {
 
         remotePlay.openFirewall = true;
         dedicatedServer.openFirewall = true;
+        localNetworkGameTransfers.openFirewall = true;
 
         extraCompatPackages = with pkgs; [proton-ge-bin];
       };
