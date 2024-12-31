77c52a6a5a82da6fd066a8a3cfe4f90c7dc58f4c
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2024-12-31 03:18:05 +0100

    refactor(overlays): source JetBrains IDEs from local versions.json file

--- overlays/jetbrains/ides/default.nix
+++ overlays/jetbrains/ides/default.nix
@@ -1,4 +1,5 @@
-{lib, ...}: let
+{ lib, ... }:
+let
   supportedIdes = [
     "clion"
     "datagrip"
@@ -14,13 +15,16 @@
     "rust-rover"
     "webstorm"
   ];
-in [
-  (final: prev: let
-    inherit (lib) optionalString;
-  in {
-    jetbrains =
-      prev.jetbrains
-      // {
+
+in
+[
+  (
+    final: prev:
+    let
+      inherit (lib) optionalString;
+    in
+    {
+      jetbrains = prev.jetbrains // {
         goland = prev.jetbrains.goland.overrideAttrs (attrs: {
           postFixup =
             (attrs.postFixup or "")
@@ -33,86 +37,107 @@ in [
             '';
         });
       };
-  })
-  (final: prev: let
-    inherit (lib) optionalAttrs attrValues toLower;
-    inherit (lib.my.mapper) toJavaProperties;
-    inherit (final) stdenv writeText;
+    }
+  )
+  (
+    final: prev:
+    let
+      inherit (lib) optionalAttrs attrValues toLower;
+      inherit (lib.my.mapper) toJavaProperties;
+      inherit (final) stdenv writeText fetchurl;
+      inherit (stdenv.hostPlatform) system;
 
-    mkPatchedJetbrainsProductDerivation = name: {
-      vmopts ? null,
-      config_path ? null,
-      caches_path ? null,
-      plugins_path ? null,
-      logs_path ? null,
-      extraProperties ? null,
-    } @ opts: let
-      shouldOverride = builtins.any (opt: opt != null) (attrValues opts);
-      ide =
-        if !prev.jetbrains ? "${name}"
-        then throw "JetBrains IDE with name ${name} is not in nixpkgs"
-        else prev.jetbrains."${name}";
+      versions = builtins.fromJSON (builtins.readFile ./versions.json);
+      products = versions."${system}" or (throw "Unsupported system: ${system}");
+
+      mkPatchedJetbrainsProductDerivation =
+        name:
+        {
+          vmopts ? null,
+          config_path ? null,
+          caches_path ? null,
+          plugins_path ? null,
+          logs_path ? null,
+          extraProperties ? null,
+        }@opts:
+        let
+          shouldOverride = builtins.any (opt: opt != null) (attrValues opts);
+          ide =
+            if !prev.jetbrains ? "${name}" then
+              throw "JetBrains IDE with name ${name} is not in nixpkgs"
+            else
+              prev.jetbrains."${name}".overrideAttrs {
+                inherit (products."${name}") version;
+
+                buildNumber = products."${name}".build_number;
+                src = fetchurl {
+                  inherit (products."${name}") url sha256;
+                };
+              };
+        in
+        if shouldOverride then
+          stdenv.mkDerivation rec {
+            inherit (ide) meta version buildNumber;
+
+            pname = meta.mainProgram + "-with-opts";
+            baseName = meta.mainProgram;
+            src = ide;
+            dontInstall = true;
+            dontFixup = true;
+            nativeBuildInputs = ide.nativeBuildInputs or [ ];
+            buildInputs = ide.buildInputs or [ ];
+
+            buildPhase =
+              let
+                rootDir =
+                  if stdenv.hostPlatform.isDarwin then
+                    "Applications/${ide.product}.app/Contents"
+                  else
+                    meta.mainProgram;
+                ideaPropertiesFile = toJavaProperties "${meta.mainProgram}" (
+                  { }
+                  // optionalAttrs (config_path != null) { "idea.config.path" = config_path; }
+                  // optionalAttrs (caches_path != null) { "idea.system.path" = caches_path; }
+                  // optionalAttrs (plugins_path != null) { "idea.plugins.path" = plugins_path; }
+                  // optionalAttrs (logs_path != null) { "idea.log.path" = logs_path; }
+                  // optionalAttrs (extraProperties != null) extraProperties
+                );
+
+                hiName = if ide.vmoptsIDE == "WEBIDE" then "WEBSTORM" else ide.vmoptsIDE;
+                loName = toLower hiName;
+                vmoptsName = loName + lib.optionalString stdenv.hostPlatform.is64bit "64" + ".vmoptions";
+                vmoptsFile = lib.optionalString (vmopts != null) (writeText vmoptsName vmopts);
+              in
+              ''
+                cp -r ${ide} $out
+                chmod +w -R $out
+
+                printf "$(cat ${ideaPropertiesFile})\n\n# Default config\n\n$(cat "$out/${rootDir}/bin/idea.properties")" > "$out/${rootDir}/bin/idea.properties"
+
+                substituteInPlace "$out/${rootDir}/bin/${loName}" \
+                  --replace-fail "${ide.vmoptsIDE}_VM_OPTIONS-'''" "${ide.vmoptsIDE}_VM_OPTIONS-'${vmoptsFile}'"
+
+                sed "s|${ide.outPath}|$out|" \
+                  -i $(realpath $out/bin/${meta.mainProgram})
+
+                if test -f "$out/bin/${meta.mainProgram}-remote-dev-server"; then
+                  sed "s|${ide.outPath}|$out|" \
+                    -i $(realpath $out/bin/${meta.mainProgram}-remote-dev-server)
+                fi
+              '';
+          }
+        else
+          ide;
     in
-      if shouldOverride
-      then
-        stdenv.mkDerivation rec {
-          inherit (ide) meta version buildNumber;
-          pname = meta.mainProgram + "-with-opts";
-          baseName = meta.mainProgram;
-          src = ide;
-          dontInstall = true;
-          dontFixup = true;
-          nativeBuildInputs = ide.nativeBuildInputs or [];
-          buildInputs = ide.buildInputs or [];
-
-          buildPhase = let
-            rootDir =
-              if stdenv.hostPlatform.isDarwin
-              then "Applications/${ide.product}.app/Contents"
-              else meta.mainProgram;
-            ideaPropertiesFile = toJavaProperties "${meta.mainProgram}" ({}
-              // optionalAttrs (config_path != null) {"idea.config.path" = config_path;}
-              // optionalAttrs (caches_path != null) {"idea.system.path" = caches_path;}
-              // optionalAttrs (plugins_path != null) {"idea.plugins.path" = plugins_path;}
-              // optionalAttrs (logs_path != null) {"idea.log.path" = logs_path;}
-              // optionalAttrs (extraProperties != null) extraProperties);
-
-            hiName =
-              if ide.vmoptsIDE == "WEBIDE"
-              then "WEBSTORM"
-              else ide.vmoptsIDE;
-            loName = toLower hiName;
-            vmoptsName =
-              loName
-              + lib.optionalString stdenv.hostPlatform.is64bit "64"
-              + ".vmoptions";
-            vmoptsFile = lib.optionalString (vmopts != null) (writeText vmoptsName vmopts);
-          in ''
-            cp -r ${ide} $out
-            chmod +w -R $out
-
-            printf "$(cat ${ideaPropertiesFile})\n\n# Default config\n\n$(cat "$out/${rootDir}/bin/idea.properties")" > "$out/${rootDir}/bin/idea.properties"
-
-            substituteInPlace "$out/${rootDir}/bin/${loName}" \
-              --replace-fail "${ide.vmoptsIDE}_VM_OPTIONS-'''" "${ide.vmoptsIDE}_VM_OPTIONS-'${vmoptsFile}'"
-
-            sed "s|${ide.outPath}|$out|" \
-              -i $(realpath $out/bin/${meta.mainProgram})
-
-            if test -f "$out/bin/${meta.mainProgram}-remote-dev-server"; then
-              sed "s|${ide.outPath}|$out|" \
-                -i $(realpath $out/bin/${meta.mainProgram}-remote-dev-server)
-            fi
-          '';
-        }
-      else ide;
-  in {
-    jetbrains =
-      prev.jetbrains
-      // (lib.listToAttrs (builtins.map (name: {
-          inherit name;
-          value = lib.makeOverridable (mkPatchedJetbrainsProductDerivation name) {};
-        })
-        supportedIdes));
-  })
+    {
+      jetbrains =
+        prev.jetbrains
+        // (lib.listToAttrs (
+          builtins.map (name: {
+            inherit name;
+            value = lib.makeOverridable (mkPatchedJetbrainsProductDerivation name) { };
+          }) supportedIdes
+        ));
+    }
+  )
 ]
