3fc136828189cef5eb9991d32407e8c0fd01c250
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2025-01-02 14:48:57 +0100

    chore(lib): export `dotfiles` for easy debugging

--- lib/default.nix
+++ lib/default.nix
@@ -5,17 +5,57 @@
   username,
   system,
   ...
-}: let
-  utils = import ./utils.nix {inherit lib pkgs username;};
-  mapper = import ./mapper.nix {inherit lib pkgs;};
-  defs = import ./defs.nix {inherit lib;};
+}:
+let
+  utils = import ./utils.nix { inherit lib pkgs username; };
+  mapper = import ./mapper.nix { inherit lib pkgs; };
+  defs = import ./defs.nix { inherit lib; };
 
   dotfiles = mapper.mapDirToAttrs ./../dotfiles;
-  scripts = import ./scripts.nix {inherit lib pkgs inputs username dotfiles;};
-in {
-  inherit utils mapper defs;
+  scripts = import ./scripts.nix {
+    inherit
+      lib
+      pkgs
+      inputs
+      username
+      dotfiles
+      ;
+  };
+in
+{
+  inherit
+    utils
+    mapper
+    defs
+    dotfiles
+    ;
 
-  hosts = import ./hosts.nix {inherit lib pkgs inputs username dotfiles scripts;};
-  desktop = import ./desktop.nix {inherit lib pkgs inputs username dotfiles scripts;};
-  overlays = import ./overlays.nix {inherit lib pkgs inputs system;};
+  hosts = import ./hosts.nix {
+    inherit
+      lib
+      pkgs
+      inputs
+      username
+      dotfiles
+      scripts
+      ;
+  };
+  desktop = import ./desktop.nix {
+    inherit
+      lib
+      pkgs
+      inputs
+      username
+      dotfiles
+      scripts
+      ;
+  };
+  overlays = import ./overlays.nix {
+    inherit
+      lib
+      pkgs
+      inputs
+      system
+      ;
+  };
 }
