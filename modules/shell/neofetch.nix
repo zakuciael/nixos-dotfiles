dc73bb19e01e469310542b66a4ee6a1468905e8c
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2025-01-02 15:18:54 +0100

    feat(shell): add dynamic disk display and image size for `neofetch`

--- modules/shell/neofetch.nix
+++ modules/shell/neofetch.nix
@@ -7,15 +7,25 @@
   ...
 }:
 let
-  inherit (lib) mkIf mkEnableOption;
+  inherit (lib)
+    mkIf
+    mkEnableOption
+    attrNames
+    concatMapStringsSep
+    length
+    ;
 
   cfg = config.modules.shell.neofetch;
   imageFile = dotfiles.neofetch."nixos.png".source;
   templateFile = dotfiles.neofetch."config.conf".source;
+
+  disks = attrNames config.fileSystems;
+  lines = 20 + (length disks);
+  lineHeight = 20;
   configFile = pkgs.runCommand "config.conf" { } ''
     substitute "${templateFile}" "$out" \
-      --replace-fail "{{% DISK_SHOW %}}" "'/'" \
-      --replace-fail "{{% IMAGE_SIZE %}}" "400px" \
+      --replace-fail "{{% DISK_SHOW %}}" "${concatMapStringsSep " " (disk: "'${disk}'") disks}" \
+      --replace-fail "{{% IMAGE_SIZE %}}" "${builtins.toString (lines * lineHeight)}px" \
       --replace-fail "{{% IMAGE_SOURCE %}}" "${imageFile}"
   '';
   neofetchPkg = pkgs.neofetch.overrideAttrs {
