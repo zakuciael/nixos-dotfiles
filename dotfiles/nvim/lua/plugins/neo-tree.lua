c8685b65479c0054abd82bca4f06c20b8c48d260
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2025-01-02 14:49:40 +0100

    chore(dotfiles): make `neo-tree` display hidden files by default

--- dotfiles/nvim/lua/plugins/neo-tree.lua
+++ dotfiles/nvim/lua/plugins/neo-tree.lua
@@ -3,5 +3,10 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
         follow_current_file = true,
+        filesystem = {
+            filtered_items = {
+                visible = true,
+            },
+        },
     },
 }
