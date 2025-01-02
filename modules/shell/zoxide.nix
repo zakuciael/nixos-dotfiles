a3fc2d6097e6d1811759648b99358e30aadf3867
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2025-01-02 19:13:37 +0100

    refactor(shell): better zoxide autocompletion for fish

--- modules/shell/zoxide.nix
+++ modules/shell/zoxide.nix
@@ -1,22 +1,40 @@
 {
   config,
   lib,
+  pkgs,
   username,
   ...
 }:
-with lib;
-with lib.my; let
+let
+  inherit (lib) mkIf mkEnableOption;
   cfg = config.modules.shell.zoxide;
-in {
+in
+{
   options.modules.shell.zoxide = {
     enable = mkEnableOption "zoxide shell integration";
   };
 
   config = mkIf (cfg.enable) {
     home-manager.users.${username} = {
-      programs.zoxide = {
-        enable = true;
-        options = ["--cmd cd"];
+      programs = {
+        fish = {
+          plugins = [
+            {
+              name = "zoxide";
+              src = pkgs.fetchFromGitHub {
+                owner = "icezyclon";
+                repo = "zoxide.fish";
+                rev = "4aa74bea2b6052eb1a301d8bffca862564a7d28a";
+                hash = "sha256-kSKFz594riTu0FmRJsbSVNdMs89br1Vl1p74cnwhnV0=";
+              };
+            }
+          ];
+        };
+        zoxide = {
+          enable = true;
+          enableFishIntegration = false;
+          options = [ "--cmd cd" ];
+        };
       };
     };
   };
