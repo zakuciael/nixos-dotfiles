f570078b8cb6da1c6ca9483191f16fe4330a6805
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2025-01-25 13:33:48 +0100

    style(modules): apply nix-fmt on `flake-parts` modules

--- modules/flake-parts/overlays.nix
+++ modules/flake-parts/overlays.nix
@@ -1,16 +1,16 @@
 # Generates "overlays" flake output using configuration found in the overlays/ directory.
-{lib, ...}: let
+{ lib, ... }:
+let
   toOverlayPath = path: ./../../overlays + builtins.toPath "/${path}.nix";
   overlays = {
     swaynotificationcenter = import (toOverlayPath "swaync");
     _1password-cli-beta = import (toOverlayPath "_1password");
     httpie-desktop = import (toOverlayPath "httpie-desktop");
   };
-in {
-  flake.overlays =
-    {
-      default = final: prev:
-        lib.fold (overlay: acc: acc // (overlay final prev)) {} (lib.attrValues overlays);
-    }
-    // overlays;
+in
+{
+  flake.overlays = {
+    default =
+      final: prev: lib.fold (overlay: acc: acc // (overlay final prev)) { } (lib.attrValues overlays);
+  } // overlays;
 }
--- modules/flake-parts/pkgs.nix
+++ modules/flake-parts/pkgs.nix
@@ -1,9 +1,12 @@
 # Generates "packages" flake output using configuration found in the pkgs/ directory.
 {
-  perSystem = {pkgs, ...}: let
-    pathToPackage = path: attrs: pkgs.callPackage (./../../pkgs + builtins.toPath "/${path}") attrs;
-  in {
-    packages.nixos-blur-plymouth = pathToPackage "nixos-blur-plymouth" {};
-    packages.controlvault2-nfc-enable = pathToPackage "controlvault2-nfc-enable" {};
-  };
+  perSystem =
+    { pkgs, ... }:
+    let
+      pathToPackage = path: attrs: pkgs.callPackage (./../../pkgs + builtins.toPath "/${path}") attrs;
+    in
+    {
+      packages.nixos-blur-plymouth = pathToPackage "nixos-blur-plymouth" { };
+      packages.controlvault2-nfc-enable = pathToPackage "controlvault2-nfc-enable" { };
+    };
 }
