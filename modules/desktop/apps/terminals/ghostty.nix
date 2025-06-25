124ea92acc5f74386cc641c64c5926ec671d6d3f
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2025-06-25 10:53:01 +0200

    refactor(apps): update `ghostty` config to newest HM & Catppuccin version

--- modules/desktop/apps/terminals/ghostty.nix
+++ modules/desktop/apps/terminals/ghostty.nix
@@ -1,6 +1,5 @@
 {
   config,
-  inputs,
   username,
   desktop,
   ...
@@ -13,32 +12,23 @@ in
   modules.desktop.wm.${desktop}.terminalPackage = cfg.package;
 
   home-manager.users.${username} = {
+    catppuccin.ghostty.enable = true;
+
     programs = {
       ghostty = {
         enable = true;
         enableBashIntegration = true; # By default enable at least bash integration.
         enableFishIntegration = config.modules.shell.fish.enable;
+        installBatSyntax = config.modules.shell.bat.enable;
 
         settings = {
           # Font settings
           font-size = 11;
           font-family = "JetBrains Mono";
 
-          # Color theme
-          theme = "catppuccin-mocha"; # TODO: Replace when catppuccin/nix will add support for Ghostty
-
           # Other
           window-decoration = false;
         };
-      };
-
-      # Install configuration sytax for bat
-      bat = {
-        syntaxes.ghostty = {
-          src = cfg.package;
-          file = "share/bat/syntaxes/ghostty.sublime-syntax";
-        };
-        config.map-syntax = [ "${hmConfig.xdg.configHome}/ghostty/config:Ghostty Config" ];
       };
     };
   };
