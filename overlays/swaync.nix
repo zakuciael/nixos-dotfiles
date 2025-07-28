ebb41bf5f11dea1f39f437dbc9b35888fb5f9c31
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2025-07-28 15:02:01 +0200

    fix(overlays): missing deps for `swaynotificationcenter` after update

--- overlays/swaync.nix
+++ overlays/swaync.nix
@@ -1,6 +1,7 @@
-{lib, ...}:
+{ lib, ... }:
 with lib;
-  singleton (final: prev: {
+singleton (
+  final: prev: {
     # More up to date version of this https://github.com/ErikReider/SwayNotificationCenter/pull/262
     swaynotificationcenter = prev.swaynotificationcenter.overrideAttrs (prevAttrs: {
       src = final.fetchFromGitHub {
@@ -9,5 +10,15 @@ with lib;
         rev = "HEAD";
         hash = "sha256-I77SYSQZkhNWgCm1WMPJULYiqQEdH4g0zWhCX547CNs=";
       };
+
+      buildInputs =
+        prevAttrs.buildInputs
+        ++ (with final; [
+          libhandy
+          cmake
+          pantheon.granite
+          gtk-layer-shell
+        ]);
     });
-  })
+  }
+)
