1ce46c94523cd760abd462401de60f27fec9a304
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2025-10-09 11:54:58 +0200

    feat(sddm): enable wayland compositor

--- modules/desktop/sddm.nix
+++ modules/desktop/sddm.nix
@@ -4,18 +4,21 @@
   ...
 }:
 with lib;
-with lib.my; let
+with lib.my;
+let
   cfg = config.modules.desktop.sddm;
-in {
+in
+{
   options.modules.desktop.sddm = {
     enable = mkEnableOption "SDDM as a display manager";
   };
 
-  config = mkIf (cfg.enable) {
+  config = mkIf cfg.enable {
     services.xserver.enable = true;
     services.displayManager = {
       sddm = {
         enable = true;
+        wayland.enable = true;
         autoNumlock = true;
       };
     };
