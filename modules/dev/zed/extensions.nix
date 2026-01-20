49149aef74c8d8bc6154b487613c1ccb9b3b086d
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2026-01-20 09:51:20 +0100

    feat(dev/zed): disable extension auto updates

--- modules/dev/zed/extensions.nix
+++ modules/dev/zed/extensions.nix
@@ -6,32 +6,40 @@
   ...
 }:
 let
-  inherit (lib.modules) mkIf;
+  inherit (lib) mkIf nameValuePair genAttrs';
   cfg = config.modules.dev.zed;
 in
 {
   config = mkIf cfg.enable {
-    home-manager.users.${username}.programs.zed-editor-extensions = {
-      enable = true;
+    home-manager.users.${username}.programs = rec {
+      zed-editor.userSettings = {
+        auto_update_extensions = genAttrs' zed-editor-extensions.packages (
+          { name, ... }: nameValuePair name false
+        );
+      };
+      zed-editor-extensions = {
+        enable = true;
 
-      packages = with pkgs.zed-extensions; [
-        (nix.overrideAttrs {
-          src = pkgs.fetchFromGitHub {
-            owner = "zakuciael";
-            repo = "zed-nix-extension";
-            rev = "main";
-            hash = "sha256-wbZ9b6zPGyG9ZVhkcLf7PQgsg5SQ9ZUVCm+8WiDSnUw=";
-          };
-        })
-        wakatime
-        discord-presence
-        catppuccin
-        catppuccin-blur-plus
-        colored-zed-icons-theme
-        material-icon-theme
-        codebook
-        comment
-      ];
+        packages = with pkgs.zed-extensions; [
+          (nix.overrideAttrs {
+            src = pkgs.fetchFromGitHub {
+              owner = "zakuciael";
+              repo = "zed-nix-extension";
+              rev = "main";
+              hash = "sha256-wbZ9b6zPGyG9ZVhkcLf7PQgsg5SQ9ZUVCm+8WiDSnUw=";
+            };
+          })
+          wakatime
+          discord-presence
+          catppuccin
+          catppuccin-blur-plus
+          colored-zed-icons-theme
+          material-icon-theme
+          codebook
+          comment
+          git-firefly
+        ];
+      };
     };
   };
 }
