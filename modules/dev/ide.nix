381293789fb4600dea8523ad588a3a080523c5b9
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2026-06-26 21:43:45 +0200

    refactor(dev/ide): update module after overlay refactor

--- modules/dev/ide.nix
+++ modules/dev/ide.nix
@@ -10,89 +10,89 @@ let
     mkOption
     mkIf
     types
-    optional
-    optionals
     ;
-  inherit (lib.asserts) assertMsg;
+  inherit (lib.my.mapper) toJavaProperties;
+
   cfg = config.modules.dev.ides;
   xdg = config.home-manager.users.${username}.xdg;
 
-  mkIdeConfig =
+  mkPropertiesFile =
     ide:
-    {
-      wayland ? true,
-      vmopts ? null,
-      extra_properties ? null,
-    }:
-    assert assertMsg (builtins.isBool wayland) "wayland is not of type 'boo'";
-    assert assertMsg (
-      vmopts == null || (builtins.isList vmopts) && (vmopts |> builtins.all (val: builtins.isString val))
-    ) "vmopts is not of type 'list of strings'";
-    assert assertMsg (
-      extra_properties == null || builtins.isAttrs extra_properties
-    ) "extra_properties is not of type 'attrs'";
     let
-      fixed_vmopts =
-        (optional wayland "-Dawt.toolkit.name=WLToolkit") ++ (optionals (vmopts != null) vmopts);
-    in
-    ide.override rec {
-      inherit extra_properties;
-      vmopts = if fixed_vmopts != [ ] then fixed_vmopts else null;
-      config_path = "${xdg.configHome}/JetBrains/${ide.pname}";
       caches_path = "${xdg.cacheHome}/JetBrains/${ide.pname}";
-      plugins_path = "${xdg.dataHome}/JetBrains/${ide.pname}";
-      logs_path = "${caches_path}/logs";
+    in
+    toJavaProperties ide.pname {
+      "idea.config.path" = "${xdg.configHome}/JetBrains/${ide.pname}";
+      "idea.system.path" = caches_path;
+      "idea.plugins.path" = "${xdg.dataHome}/JetBrains/${ide.pname}";
+      "idea.log.path" = "${caches_path}/logs";
     };
 
   ides =
     with pkgs.jetbrains;
     [
-      (mkIdeConfig clion { })
-      (mkIdeConfig datagrip { })
-      (mkIdeConfig dataspell { })
-      (mkIdeConfig gateway { })
-      (mkIdeConfig goland { })
-      (mkIdeConfig idea { })
-      (mkIdeConfig mps { })
-      (mkIdeConfig phpstorm { })
-      (mkIdeConfig pycharm { })
-      (mkIdeConfig rider { })
-      (mkIdeConfig ruby-mine { })
-      (mkIdeConfig rust-rover { })
-      (mkIdeConfig webstorm { })
+      clion
+      datagrip
+      dataspell
+      gateway
+      goland
+      idea
+      mps
+      phpstorm
+      pycharm
+      rider
+      ruby-mine
+      rust-rover
+      webstorm
     ]
-    |> builtins.map (ide: {
-      name = ide.baseName or ide.pname;
-      value = ide;
+    |> map (ide: {
+      name = ide.pname;
+      value = (ide.override { forceWayland = true; }).overrideAttrs (
+        finalAttrs:
+        let
+          propertiesFile = mkPropertiesFile ide;
+        in
+        {
+          postInstall = (finalAttrs.postInstall or "") + ''
+            # Add custom properties file to the install directory
+            printf "$(cat ${propertiesFile})\n\n# Default config\n\n$(cat $(makeWritable "$out/${finalAttrs.pname}/bin/idea.properties"))" > "$out/${finalAttrs.pname}/bin/idea.properties"
+          '';
+        }
+      );
     })
-    |> builtins.listToAttrs;
-  installed_ides = cfg |> builtins.map (name: ides."${name}");
+    |> lib.listToAttrs;
+
+  installed_ides = cfg |> map (name: ides."${name}");
 in
 {
-  options.modules.dev.ides = mkOption {
-    description = "A list of JetBrains IDEs names to install";
-    example = [
-      "rust-rover"
-      "webstorm"
-    ];
-    default = [ ];
-    type = ides |> builtins.attrNames |> types.enum |> types.listOf;
+  options.modules = {
+    test = mkOption {
+      type = types.listOf types.package;
+      default = installed_ides;
+    };
+    dev.ides = mkOption {
+      description = "A list of JetBrains IDEs names to install";
+      example = [
+        "rust-rover"
+        "webstorm"
+      ];
+      default = [ ];
+      type = ides |> builtins.attrNames |> types.enum |> types.listOf;
+    };
   };
 
   config = mkIf (cfg != [ ]) {
     home-manager.users.${username} = {
-      home = {
-        packages = installed_ides;
+      home.packages = installed_ides;
 
-        # Make symlinks for rofi-jetbrains plugin
-        file =
-          installed_ides
-          |> builtins.map (ide: {
-            name = ".local/share/JetBrains/apps/${ide.pname}";
-            value.source = "${ide}/${ide.meta.mainProgram}";
-          })
-          |> builtins.listToAttrs;
-      };
+      # Make symlinks for rofi-jetbrains plugin
+      xdg.dataFile =
+        installed_ides
+        |> map (ide: {
+          name = "JetBrains/apps/${ide.pname}";
+          value.source = "${ide}/${ide.pname}";
+        })
+        |> lib.listToAttrs;
     };
   };
 }
