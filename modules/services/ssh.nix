79ce04e977ffd95c8a6931d23113b795b4ccb3c4
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2026-07-21 16:22:31 +0200

    fix(services/ssh): `matchBlocks` warnings

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -112,17 +112,17 @@ in
       enable = true;
       enableDefaultConfig = false;
 
-      matchBlocks."*" = {
-        forwardAgent = false;
-        addKeysToAgent = "no";
-        compression = false;
-        serverAliveInterval = 0;
-        serverAliveCountMax = 3;
-        hashKnownHosts = false;
-        userKnownHostsFile = "~/.ssh/known_hosts";
-        controlMaster = "no";
-        controlPath = "~/.ssh/master-%r@%n:%p";
-        controlPersist = "no";
+      settings."*" = {
+        ForwardAgent = false;
+        AddKeysToAgent = "no";
+        Compression = false;
+        ServerAliveInterval = 0;
+        ServerAliveCountMax = 3;
+        HashKnownHosts = false;
+        UserKnownHostsFile = "~/.ssh/known_hosts";
+        ControlMaster = "no";
+        ControlPath = "~/.ssh/master-%r@%n:%p";
+        ControlPersist = "no";
       };
 
       includes = singleton config.sops.templates."ssh/hosts.conf".path;
