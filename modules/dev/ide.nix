c009e6b1f167f3ca5e87d4f41e44e37179d8710a
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2026-07-21 18:42:24 +0200

    feat(dev/ide): add files for JetBrains Toolbox-like compatibility

--- modules/dev/ide.nix
+++ modules/dev/ide.nix
@@ -64,14 +64,27 @@ in
     home-manager.users.${username} = {
       home.packages = installed_ides;
 
-      # Make symlinks for rofi-jetbrains plugin
       xdg.dataFile =
         installed_ides
-        |> map (ide: {
-          name = "JetBrains/apps/${ide.pname}";
-          value.source = "${ide}/${ide.pname}";
-        })
-        |> lib.listToAttrs;
+        |> map (
+          ide:
+          let
+            productInfo = lib.importJSON "${ide}/${ide.pname}/product-info.json";
+            inherit (productInfo) svgIconPath;
+            inherit (builtins.elemAt productInfo.launch 0) launcherPath startupWmClass;
+            scriptName = lib.removePrefix "bin/" launcherPath;
+          in
+          {
+            "JetBrains/Toolbox/scripts/${scriptName}".source = lib.getExe ide;
+            "JetBrains/Toolbox/apps/${ide.pname}".source = "${ide}/${ide.pname}";
+            "JetBrains/Toolbox/dotDesktopIcons/${startupWmClass}-dummy.desktop.icon.svg".source =
+              "${ide}/${ide.pname}/${svgIconPath}";
+
+            # Make symlinks for rofi-jetbrains plugin
+            "JetBrains/apps/${ide.pname}".source = "${ide}/${ide.pname}";
+          }
+        )
+        |> lib.foldl' (acc: curr: lib.recursiveUpdate acc curr) { };
     };
   };
 }
